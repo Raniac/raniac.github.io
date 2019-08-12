324fbf8ea78bcef1510955b90d6986c77bc5ffff
Benny Ray <leibingye@outlook.com>
2019-08-12 09:44:27 +0800

    Modified contact page.

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="page-area">
-    <div style="background-color: #00ccff; color: #fff; margin: auto; height: 1000px; width: auto"><h1>My contact will show here!</h1></div>
+    <div style="background-color: #E8E8E8; color: #000000; margin: auto; height: 200px; width: auto">
+      <span style="background-color: #00CCFF; width: 8px; height: 200px; float: left"></span>
+      <h1>My contact will show here!</h1>
+    </div>
   </div>
 </template>
 
--- src/components/pages/Projects.vue
+++ src/components/pages/Projects.vue
@@ -14,6 +14,5 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
 </style>

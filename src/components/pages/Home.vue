6323019a7b6488b65382482f05c46a57ca809582
Benny Ray <leibingye@outlook.com>
2019-08-12 13:56:02 +0800

    Modified home and projects page.

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="page-area">
-    <div style="margin: 10px auto; width: 1000px; height: 1000px; box-shadow: 1px 0 10px #E8E8E8">
-      <img src="@/assets/img/Vegito.jpg" style="width: 160px; height: 160px; margin: 20px 40px; float: left"/>
+    <div style="margin: 10px auto; width: 1000px; height: 1000px; text-align: left; box-shadow: 1px 0 10px #E8E8E8">
+      <img src="@/assets/img/Vegito.jpg" style="width: 1000px; height: 100px; margin: 0; float: left"/>
+      <div style="margin: 10px">Hi! I'm Benny Ray!</div>
     </div>
   </div>
 </template>

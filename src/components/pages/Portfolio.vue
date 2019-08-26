f3cd842d8f9dea8cf401b9cfad7eaeda52281d75
Benny Ray <leibingye@outlook.com>
2019-08-26 21:09:46 +0800

    Modified portfolio.

--- src/components/pages/Portfolio.vue
+++ src/components/pages/Portfolio.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="page-area">
     <div class="project-card">
-      <img src="@/assets/img/Vegito.jpg" style="width: 160px; height: 160px; margin: 20px 40px; float: left"/>
-      <div>null</div>
+      <span style="background-color: #282828; width: 4px; height: 120px; float: right"></span>
+      <img src="https://raw.githubusercontent.com/Raniac/NEURO-LEARN/master/doc/img/UI.png" style="height: 400px; width: auto; float: left"/>
+      <el-tooltip content="Check out the repository at GITHUB" placement="left" style="float: right; margin: 40px 40px 0 0; font-size: 40px; color: #505050">
+        <i class="el-icon-connection" @click="handleRedirect('nlw-git')"></i>
+      </el-tooltip>
+      <el-tooltip content="Check out the home page" placement="left" style="float: right; margin: 40px 20px 0 0; font-size: 40px; color: #505050">
+        <i class="el-icon-s-promotion" @click="handleRedirect('nlw-home')"></i>
+      </el-tooltip>
+      <div style="float: left; width: 50%">
+        <div class="project-title"><i class="el-icon-folder-opened"></i> NEURO-LEARN</div>
+        <div class="project-intro">A decentralized solution for pattern analysis of neuroimaging data.</div>
+      </div>
     </div>
     <div class="project-card">
-      <img src="@/assets/img/Vegito.jpg" style="width: 160px; height: 160px; margin: 20px 40px; float: left"/>
-      <div>null</div>
-    </div>
-    <div class="project-card">
-      <img src="@/assets/img/Vegito.jpg" style="width: 160px; height: 160px; margin: 20px 40px; float: left"/>
-      <div>null</div>
-    </div>
-    <div class="project-card">
-      <img src="@/assets/img/Vegito.jpg" style="width: 160px; height: 160px; margin: 20px 40px; float: left"/>
-      <div>null</div>
+      <span style="background-color: #282828; width: 4px; height: 120px; float: right"></span>
+      <el-tooltip content="Check out the repository at GITHUB" placement="left" style="float: right; margin: 40px 40px 0 0; font-size: 40px; color: #505050">
+        <i class="el-icon-connection" @click="handleRedirect('np-git')"></i>
+      </el-tooltip>
+      <div style="float: left; width: 80%">
+        <div class="project-title"><i class="el-icon-folder-opened"></i> neuroimaging_with_python</div>
+        <div class="project-intro">Neuroimaging data analysis using ML tools with Python.</div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,6 +33,17 @@ export default {
   data () {
     return {
     }
+  },
+  methods: {
+    handleRedirect (loc) {
+      if (loc === 'nlw-git') {
+        window.open('https://github.com/Raniac/NEURO-LEARN')
+      } else if (loc === 'nlw-home') {
+        window.open('https://raniac.github.io/neurolearn_demo/')
+      } else if (loc === 'np-git') {
+        window.open('https://github.com/Raniac/neuroimaging_with_python')
+      }
+    }
   }
 }
 </script>
@@ -33,10 +52,24 @@ export default {
 .project-card {
   margin: 20px auto;
   width: 1000px;
-  height: 340px;
+  height: 400px;
   box-shadow: 0 0 10px #808080;
   &:hover {
     box-shadow: 0 0 20px #808080;
+  }
+  .project-title {
+    text-align: left;
+    margin: 40px 40px 20px 40px;
+    font-size: 34px;
+    font-family: Georgia, Palatino, serif;
+  }
+  .project-intro {
+    line-height: 30px;
+    color: #505050;
+    text-align: left;
+    margin: 0 40px;
+    font-size: 28px;
+    font-family: serif;
   }
 }
 </style>

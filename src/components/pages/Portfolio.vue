a0ff21f730987137b8fc831491debd780fedb0da
Benny Ray <leibingye@outlook.com>
2019-09-01 21:46:46 +0800

    Fixed minor issue.

--- src/components/pages/Portfolio.vue
+++ src/components/pages/Portfolio.vue
@@ -3,7 +3,7 @@
     <div class="page-info">
       <span style="background-color: #282828; margin-top: 15px; width: 8px; height: 44px; float: left"></span>
       <div style="font-size: 50px; color: #282828; line-height: 150%">&ensp;PORTFOLIO</div>
-      <div style="font-size: 24px; color: #505050; line-height: 150%; font-family: Georgia, Palatino, serif">&ensp;Here are the projects that I've working on.</div>
+      <div style="font-size: 24px; color: #505050; line-height: 150%; font-family: Georgia, Palatino, serif">&ensp;Here are the projects that I've been working on.</div>
     </div>
     <div class="project-card">
       <span style="background-color: #282828; width: 4px; height: 200px; float: right"></span>
